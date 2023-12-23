0ab92f0ed8099dc7eae4642e7ab7924bfd6ce0ac
andrii-koriak <koriak.andruha@gmail.com>
2023-12-23 20:53:50 +0200

    feature:add firebase auth

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import auth
 
 cred = credentials.Certificate("credentials.json")
 firebase_admin.initialize_app(cred)
+
+email = input("write your email")
+password = input("write your password")
+
+user = auth.create_user(email=email, password=password)
